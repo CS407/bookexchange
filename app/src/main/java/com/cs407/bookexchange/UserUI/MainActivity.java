7b0269dfbecb44885376fee77e1f8457b8291f72
Eastin <ewbricco@gmail.com>
2016-03-07 22:32:18 -0600

    Added empty activities and corresponding views for login and main.

---
+++ app/src/main/java/com/cs407/bookexchange/UserUI/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cs407.bookexchange.UserUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cs407.bookexchange.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
