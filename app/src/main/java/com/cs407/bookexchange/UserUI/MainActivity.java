8077c259754c8bdae83c1edc0730f6d850dc7862
Donnacaihahaha <zcai28@wisc.edu>
2016-03-08 07:07:08 -0600

    third commit with main activity ui, with login and register activities

--- app/src/main/java/com/cs407/bookexchange/UserUI/MainActivity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cs407.bookexchange.UserUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import com.cs407.bookexchange.R;
@@ -14,6 +16,11 @@ Button registerButton;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         registerButton =(Button)findViewById(R.id.registerButton);
-
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+            Intent in = new Intent(getApplicationContext(), login.class);
+            startActivity(in);
+            }
+        });
     }
 }
--- app/src/main/java/com/cs407/bookexchange/UserUI/login.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/login.java
@@ -1,15 +1,26 @@
 package com.cs407.bookexchange.UserUI;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
 
 import com.cs407.bookexchange.R;
 
 public class login extends AppCompatActivity {
-
+Button checkButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        checkButton =(Button)findViewById(R.id.registerButton);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //how to check userinfo
+            }
+        });
     }
+
 }
