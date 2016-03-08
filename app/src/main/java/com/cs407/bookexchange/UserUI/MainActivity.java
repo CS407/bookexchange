4083ac05f85a3e21b1ff96e1478c349a0fde4198
Donnacaihahaha <zcai28@wisc.edu>
2016-03-08 06:40:57 -0600

    second commit with main activity ui, need further design of logo and bookexchange picture

--- app/src/main/java/com/cs407/bookexchange/UserUI/MainActivity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/MainActivity.java
@@ -1,15 +1,19 @@
 package com.cs407.bookexchange.UserUI;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.cs407.bookexchange.R;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerButton =(Button)findViewById(R.id.registerButton);
+
     }
 }
