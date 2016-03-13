7ff3343e2032541be14fc3474bc585625b80b803
Sunny Shah <shahsunny712@gmail.com>
2016-03-13 17:30:45 -0600

    Added package that will handle connection of the UI with the backend.
    The new package ('connectors') will have several packages - one for each
    type of CRUD object, with each such package containing a class extending
    an AsyncTask. Each such class will call a different type of database
    operation (self-evident from the name of the file) in a non-UI thread.
    
    The AsyncTasks will be called from the UI threads.

---
+++ app/src/main/java/com/cs407/bookexchange/connectors/users/CreateUserConnector.java
@@ -0,0 +1,28 @@
+package com.cs407.bookexchange.connectors.users;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by ssunny7 on 3/12/2016.
+ */
+public class CreateUserConnector extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+}
