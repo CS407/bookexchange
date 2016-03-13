4c297607fdfc104faffdcd43b4d6490f567189a6
Sunny Shah <shahsunny712@gmail.com>
2016-03-13 17:53:40 -0600

    Defined types for CreateUserConnector AsyncTask

--- app/src/main/java/com/cs407/bookexchange/connectors/users/CreateUserConnector.java
+++ app/src/main/java/com/cs407/bookexchange/connectors/users/CreateUserConnector.java
@@ -2,18 +2,23 @@ package com.cs407.bookexchange.connectors.users;
 
 import android.os.AsyncTask;
 
+import com.cs407.bookexchange.db.Constants;
+import com.cs407.bookexchange.db.Create;
+
+import java.util.HashMap;
+
 /**
  * Created by ssunny7 on 3/12/2016.
  */
-public class CreateUserConnector extends AsyncTask<Void, Void, Void> {
+public class CreateUserConnector extends AsyncTask<HashMap<String, String>, Void, Boolean> {
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPostExecute(Boolean retVal) {
+        super.onPostExecute(retVal);
     }
 
     @Override
@@ -22,7 +27,9 @@ public class CreateUserConnector extends AsyncTask<Void, Void, Void> {
     }
 
     @Override
-    protected Void doInBackground(Void... params) {
-        return null;
+    protected Boolean doInBackground(HashMap<String, String>... params) {
+        boolean retVal = Create.executeCreate(Constants.CRUDObject.USER, params[0]);
+
+        return new Boolean(retVal);
     }
 }
