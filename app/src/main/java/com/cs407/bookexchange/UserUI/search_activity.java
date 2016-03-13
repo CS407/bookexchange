87868d9828ac58bb8507c5abe38fe01a5b2d67c1
Donnacaihahaha <zcai28@wisc.edu>
2016-03-12 18:50:10 -0600

    add new activities for adding and searching books

---
+++ app/src/main/java/com/cs407/bookexchange/UserUI/search_activity.java
@@ -0,0 +1,16 @@
+package com.cs407.bookexchange.UserUI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cs407.bookexchange.R;
+
+public class search_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_activity);
+
+    }
+}
---
+++ app/src/main/java/com/cs407/bookexchange/UserUI/seller_manager_activity.java
@@ -0,0 +1,15 @@
+package com.cs407.bookexchange.UserUI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cs407.bookexchange.R;
+
+public class seller_manager_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_manager_activity);
+    }
+}
