c24547bad845939ceab048241f064ffe2f88c910
Eastin <ewbricco@gmail.com>
2016-03-12 18:51:46 -0600

    Got data from login

--- app/src/main/java/com/cs407/bookexchange/UserUI/login.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/login.java
@@ -1,26 +1,39 @@
 package com.cs407.bookexchange.UserUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cs407.bookexchange.R;
 
 public class login extends AppCompatActivity {
 Button checkButton;
+    String username; //name that user entered for login
+    String password; //password that user entered
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        checkButton =(Button)findViewById(R.id.registerButton);
-        /*checkButton.setOnClickListener(new View.OnClickListener() {
+
+        checkButton =(Button)findViewById(R.id.loginButton1);
+        checkButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //Intent i = new Intent
+                EditText userField= (EditText) findViewById(R.id.username);
+                EditText userPass = (EditText) findViewById(R.id.password);
+                username = userField.getText().toString();
+                password = userPass.getText().toString();
+                Toast.makeText(getBaseContext(),"Logged In as " + username,
+                        Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
             }
-        });*/
+        });
     }
 
 }
