85c969bc39237cc5cc12dcf93eb8688c5ceead63
Eastin <ewbricco@gmail.com>
2016-03-13 16:27:38 -0500

    Linked search and sell manager.

--- app/src/main/java/com/cs407/bookexchange/UserUI/search_activity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/search_activity.java
@@ -1,7 +1,10 @@
 package com.cs407.bookexchange.UserUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.cs407.bookexchange.R;
 
@@ -12,5 +15,15 @@ public class search_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_activity);
 
+
+        Button managerButton =(Button)findViewById(R.id.managerButton);
+        managerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(), seller_manager_activity.class);
+                startActivity(in);
+            }
+        });
+
     }
+
 }
