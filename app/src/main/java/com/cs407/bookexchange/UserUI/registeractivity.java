e285070e7e124cda01c955d8b310c8482523bd77
Eastin <ewbricco@gmail.com>
2016-03-13 17:44:58 -0500

    Added additional methods to register.

--- app/src/main/java/com/cs407/bookexchange/UserUI/registeractivity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/registeractivity.java
@@ -15,6 +15,9 @@ public class registeractivity extends AppCompatActivity {
     private String email;
     private String password;
     private String password2;
+    private int zip;
+    private String name;
+    private long phoneNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +31,16 @@ public class registeractivity extends AppCompatActivity {
                 EditText emailField = (EditText) findViewById(R.id.editText2);
                 EditText passField = (EditText) findViewById(R.id.editText3);
                 EditText pass2Field = (EditText) findViewById(R.id.editText4);
+                EditText nameField = (EditText) findViewById(R.id.editText5);
+                EditText phoneField = (EditText) findViewById(R.id.editText7);
+                EditText zipField = (EditText) findViewById(R.id.editText6);
                 username = userField.getText().toString();
                 email = emailField.getText().toString();
                 password = passField.getText().toString();
                 password2 = pass2Field.getText().toString();
+                name = nameField.getText().toString();
+                zip = Integer.parseInt(zipField.getText().toString());
+                phoneNum = Long.parseLong(phoneField.getText().toString());
                 if(password.equals(password2)) {
                     Toast.makeText(getBaseContext(), "Successfully registered as " + username,
                             Toast.LENGTH_SHORT).show();
