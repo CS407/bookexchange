d80978e2fdf1e187260b861b362ef032d7c27bd7
Eastin <ewbricco@gmail.com>
2016-03-13 16:44:32 -0500

    nothing

--- app/src/main/java/com/cs407/bookexchange/UserUI/new_listing_activity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/new_listing_activity.java
@@ -1,7 +1,10 @@
 package com.cs407.bookexchange.UserUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.cs407.bookexchange.R;
 
@@ -12,5 +15,12 @@ public class new_listing_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_listing_activity);
 
+        Button =(Button)findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(), registeractivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
