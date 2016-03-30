98f84b5d075bcda76963b5688930128c0d53973f
Sunny Shah <shahsunny712@gmail.com>
2016-03-29 21:42:42 -0500

    User prefernces read and write
    Added a class exposing static methods to set/get user preferences (stored
    via SharedPreferences). This will store, among other things, the curently
    logged in user.

---
+++ app/src/main/java/com/cs407/bookexchange/userprefs/Constants.java
@@ -0,0 +1,10 @@
+package com.cs407.bookexchange.userprefs;
+
+/**
+ * Created by ssunny7 on 3/29/2016.
+ */
+public class Constants {
+    public static String PREFS_NAME = "be_preferences";
+
+    public static String PREF_CUR_USER = "cur_user";
+}
---
+++ app/src/main/java/com/cs407/bookexchange/userprefs/UserPrefs.java
@@ -0,0 +1,27 @@
+package com.cs407.bookexchange.userprefs;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by ssunny7 on 3/29/2016.
+ */
+public class UserPrefs {
+    private static SharedPreferences preferences = null;
+    private static Context context = null;
+
+    public static void init(Context _context) {
+        context = _context;
+        preferences = context.getSharedPreferences(Constants.PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static String readPreference(String name) {
+        return preferences.getString(name, null);
+    }
+
+    public static void writePreference(String name, String value) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(name, value);
+        editor.commit();
+    }
+}
