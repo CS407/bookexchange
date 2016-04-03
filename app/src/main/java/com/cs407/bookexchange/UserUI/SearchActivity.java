7f1d88ced989efb26aeca555bd0b09bf8826a5d0
Katie Dudenas <kdudenas@users.noreply.github.com>
2016-04-03 14:57:16 -0500

    Basic setup for search-->results action; added search button onClick that goes to ResultsActivity. Added text field to ResultsActivity xml so I knew what I was looking at :) Added book connector classes to be filled in

--- app/src/main/java/com/cs407/bookexchange/UserUI/SearchActivity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/SearchActivity.java
@@ -3,6 +3,7 @@ package com.cs407.bookexchange.UserUI;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,11 +16,22 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-
         Button managerButton =(Button)findViewById(R.id.managerButton);
         managerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent in = new Intent(getApplicationContext(), SellerManagerActivity.class); //go to results
+                Intent in = new Intent(getApplicationContext(), SellerManagerActivity.class);
+                startActivity(in);
+            }
+        });
+
+        Button searchButton =(Button)findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("SearchActivity", "CLicked search");
+
+                //get search results
+
+                Intent in = new Intent(getApplicationContext(), ResultsActivity.class);
                 startActivity(in);
             }
         });
