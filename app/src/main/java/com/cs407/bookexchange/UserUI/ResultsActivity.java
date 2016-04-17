97ee344c38f358f239fe48983e59c008377a6e7d
Katie Dudenas <kdudenas@users.noreply.github.com>
2016-04-16 19:37:24 -0500

    updated ResultsActivity; search results are now displayed. TODO: make it pretty; eventually add functionality to get book details?

--- app/src/main/java/com/cs407/bookexchange/UserUI/ResultsActivity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/ResultsActivity.java
@@ -4,39 +4,54 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.cs407.bookexchange.R;
 import com.cs407.bookexchange.db.Book;
 import com.cs407.bookexchange.db.Constants;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ResultsActivity extends AppCompatActivity {
 
-    //Class fields here
+    ListView resultsView;
     ArrayList<Book> books;
     ArrayList<Book> resultBooks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-
-        //TODO SearchActivity will pass in an ArrayList<Book>
-        books = new ArrayList<Book>(); //add books to test display
+        resultsView = (ListView)findViewById(R.id.searchResultsList);
+        //books = new ArrayList<Book>(); //add books to test display
 
         Intent searchIntent = getIntent();
         boolean foundBooks = searchIntent.getBooleanExtra(Constants.BOOK_SEARCH_STATUS, false);
         if(foundBooks){
             resultBooks = searchIntent.getParcelableArrayListExtra(Constants.BOOKS_RESULTS_KEY);
             Log.w("{RESACT}", resultBooks.toString());
+
+            ArrayAdapter<Book> listAdapter = new ArrayAdapter<Book>(this, android.R.layout.simple_list_item_1, resultBooks);
+            resultsView.setAdapter(listAdapter);
+
+
+
+
         }
         else{
             //TODO
         }
 
-
-
-
-
     }
+
+//    public class ResultsAdapter extends ArrayAdapter{
+//
+//
+//
+//    }
+
+
+
+
 }
