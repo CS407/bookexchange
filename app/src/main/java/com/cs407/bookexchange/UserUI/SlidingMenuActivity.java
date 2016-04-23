81ba6fdb0e5a8cd8a1971e21fdbdf71c094f809a
Eastin <ewbricco@gmail.com>
2016-04-23 16:41:01 -0500

    fixed spacing on new listing page.

---
+++ app/src/main/java/com/cs407/bookexchange/UserUI/SlidingMenuActivity.java
@@ -0,0 +1,146 @@
+package com.cs407.bookexchange.UserUI;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.app.Fragment;
+//import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.cs407.bookexchange.R;
+import com.cs407.bookexchange.adapter.SlidingMenuAdapter;
+import com.cs407.bookexchange.fragment.Fragment1;
+import com.cs407.bookexchange.model.ItemSlideMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlidingMenuActivity extends ActionBarActivity {
+
+    private List<ItemSlideMenu> listSliding;
+    private SlidingMenuAdapter adapter;
+    private ListView listViewSliding;
+    private DrawerLayout drawerLayout;
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slidingmenu);
+
+        listViewSliding=(ListView)findViewById(R.id.lv_sliding_menu);
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+
+        listSliding=new ArrayList<>();
+
+
+        listSliding.add(new ItemSlideMenu(R.mipmap.ic_launcher,"Setting"));
+        adapter= new SlidingMenuAdapter(this,listSliding);
+        listViewSliding.setAdapter(adapter);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        if(getActionBar() == null){
+            Log.d("problem", "ah");
+        }
+
+        //set title
+        setTitle(listSliding.get(0).getTitle());
+        //item selected
+        listViewSliding.setItemChecked(0, true);
+        //close menu
+        drawerLayout.closeDrawer(listViewSliding);
+
+
+        //display fragment when start
+        replaceFragment(0);
+
+        listViewSliding.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                setTitle(listSliding.get(position).getTitle());
+                listViewSliding.setItemChecked(position, true);
+
+                //replace fragment
+                replaceFragment(position);
+
+
+                drawerLayout.closeDrawer(listViewSliding);
+            }
+
+        });
+
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_opened,R.string.drawer_closed){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+
+    private void replaceFragment(int pos){
+
+        Fragment fragment = null;
+
+        switch(pos){
+            case 0:
+                fragment = new Fragment1();
+                break;
+            default:
+                fragment=new Fragment1();
+                break;
+
+        }
+        if(null!=fragment){
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.main_content,fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+    }
+
+}
+
+
--- app/src/main/java/com/cs407/bookexchange/fragment/Fragment1.java
+++ app/src/main/java/com/cs407/bookexchange/fragment/Fragment1.java
@@ -1,7 +1,8 @@
 package com.cs407.bookexchange.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+//import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
