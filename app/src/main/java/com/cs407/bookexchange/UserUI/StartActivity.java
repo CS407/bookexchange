f39ba4cb5f1e631f33c7a6a68ce06fbf412d2461
Sunny Shah <shahsunny712@gmail.com>
2016-04-27 15:01:54 -0500

    Added check at the launch of the start activity to quit the app if Internet is not available.

--- app/src/main/java/com/cs407/bookexchange/UserUI/StartActivity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/StartActivity.java
@@ -1,7 +1,12 @@
 package com.cs407.bookexchange.UserUI;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +16,7 @@ import com.cs407.bookexchange.userprefs.Constants;
 import com.cs407.bookexchange.userprefs.UserPrefs;
 
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends Activity {
 
     Button loginButton;
     Button registerButton;
@@ -21,14 +26,27 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        if(!isNetWorkAvailable()) {
+            new AlertDialog.Builder(StartActivity.this)
+                    .setTitle("Internet not available!")
+                    .setMessage("Please switch on the Internet before launching this app.")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+        }
+
         UserPrefs.init(getApplicationContext());
-        if(UserPrefs.readPreference(Constants.PREF_CUR_USER_USERNAME) != null) {
+        if (UserPrefs.readPreference(Constants.PREF_CUR_USER_USERNAME) != null) {
             Intent searchIntent = new Intent(getApplicationContext(), SearchActivity.class);
             startActivity(searchIntent);
             finish();
         }
 
-        loginButton = (Button)findViewById(R.id.btnLoginStartActivity);
+        loginButton = (Button) findViewById(R.id.btnLoginStartActivity);
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intentLogin = new Intent(getApplicationContext(), LoginActivity.class);
@@ -37,12 +55,18 @@ public class StartActivity extends AppCompatActivity {
             }
         });
 
-        registerButton = (Button)findViewById(R.id.btnRegisterStartActivity);
+        registerButton = (Button) findViewById(R.id.btnRegisterStartActivity);
         registerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intentRegister = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intentRegister);
             }
         });
+    }
+
+    private boolean isNetWorkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        return info != null && info.isConnectedOrConnecting();
     }
 }
