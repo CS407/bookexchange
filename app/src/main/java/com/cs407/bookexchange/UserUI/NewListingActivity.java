b3dc65a3f6b93dca8eeec8daf0ff3f0af200546c
Sunny Shah <shahsunny712@gmail.com>
2016-04-30 00:14:01 -0500

    Added spinner for choosing book condition

--- app/src/main/java/com/cs407/bookexchange/UserUI/NewListingActivity.java
+++ app/src/main/java/com/cs407/bookexchange/UserUI/NewListingActivity.java
@@ -1,11 +1,11 @@
 package com.cs407.bookexchange.UserUI;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.cs407.bookexchange.R;
@@ -16,7 +16,8 @@ import java.util.HashMap;
 
 public class NewListingActivity extends AppCompatActivity {
 
-    private EditText etTitle, etComments, etISBN, etAuthors, etCondition, etEdition, etDept, etPrice, etCourseNo;
+    private EditText etTitle, etComments, etISBN, etAuthors, etEdition, etDept, etPrice, etCourseNo;
+    private Spinner spnCondition;
     private Button btnAdd;
 
     @Override
@@ -27,7 +28,7 @@ public class NewListingActivity extends AppCompatActivity {
         etTitle = (EditText) findViewById(R.id.etTitleNewListingActivity);
         etAuthors = (EditText) findViewById(R.id.etAuthorsNewListingActivity);
         etComments = (EditText) findViewById(R.id.etCommentsNewListingActivity);
-        etCondition = (EditText) findViewById(R.id.etConditionNewListingActivity);
+        spnCondition = (Spinner) findViewById(R.id.spnConditionNewListingActivity);
         etPrice = (EditText) findViewById(R.id.etPriceNewListingActivity);
         etCourseNo = (EditText) findViewById(R.id.etClassNewListingActivity);
         etDept = (EditText) findViewById(R.id.etDepartmentNewListingActivity);
@@ -40,7 +41,7 @@ public class NewListingActivity extends AppCompatActivity {
                 String title = etTitle.getText().toString();
                 String authors = etAuthors.getText().toString();
                 String comments = etComments.getText().toString();
-                String condition = etCondition.getText().toString();
+                String condition = spnCondition.getSelectedItem().toString();
                 Float price = null;
                 if (!etPrice.getText().toString().isEmpty())
                     price = Float.parseFloat(etPrice.getText().toString());
